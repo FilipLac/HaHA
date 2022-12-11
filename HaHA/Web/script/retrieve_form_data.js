8a00548de53f54dd8791acc9f34a0563640bb4b2
Michal Adam <Michal.Adam@student.soskolin.cz>
2022-12-11 18:10:49 +0100

    added one screen, gallery rework

--- HaHA/Web/script/retrieve_form_data.js
+++ HaHA/Web/script/retrieve_form_data.js
@@ -30,47 +30,21 @@ function datata()
     postavy = document.getElementById("postavy").value;
     pripominka = document.getElementById("pripominka").value;
     const stolenDataArray = [
-        {
-          name: jmeno ,
-          surname: prijmeni,
-          imail: email,
-          phone: telefon,
-          theme: tema,
-          story: pribeh,
-          place: prostor,
-          difficulty: obtiznost,
-          atmosphere: atmosfera,
-          characters: postavy,
-          remarks: pripominka
-        }
-      ]
-    
-    //vytvoření CSV souboru
-    const titleKeys = Object.keys(stolenDataArray[0])
+    {
+      name: jmeno ,
+      surname: prijmeni,
+      imail: email,
+      phone: telefon,
+      theme: tema,
+      story: pribeh,
+      place: prostor,
+      difficulty: obtiznost,
+      atmosphere: atmosfera,
+      characters: postavy,
+      remarks: pripominka
+    }
+  ]
+//dále se mohou data posílat někam pryč na server... ukrást je hezky uživatelům
 
-    const refinedData = []
-    refinedData.push(titleKeys)
-
-    stolenDataArray.forEach(item => {
-        refinedData.push(Object.values(item))  
-    })
-
-    refinedData.forEach(row => {
-        csvContent += row.join(';') + '\n'
-    })
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8,' })
-    const objUrl = URL.createObjectURL(blob)
-}
-
-//stažení souboru, i know je to hodně weird způsob, jak to nakódit :3
-function hackanddownloaddata()
-{
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8,' })
-    const objUrl = URL.createObjectURL(blob)
-    const link = document.createElement('a')
-    link.setAttribute('href', objUrl)
-    link.setAttribute('download', 'File.csv')
-    link.textContent = 'Click to Download'
-
-    document.getElementById("test").append(link)
+    window.location.replace("thankyou.html");
 }
